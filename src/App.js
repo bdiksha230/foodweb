39097c4ceb8c4c244af8dc410b1be4d476100ba7
Diksha Bhagat <90504015+bdiksha230@users.noreply.github.com>
2024-12-15 01:17:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { Navbar } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
